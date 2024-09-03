8ff1d599d6a90882526070adb7ae31f0bc2c8f11
John Graham Reynolds <john.graham.reynolds@vumc.org>
2024-09-03 23:52:46 +0000

    rename first query to hello world to reflect its basic introduction

--- src/interactive_model/queries/hello_world.py
+++ src/interactive_model/queries/hello_world.py
@@ -2,13 +2,15 @@ from src.model_construction import ClaudeModel
 
 if __name__ == "__main__":
 
+    """ update ANTHROPIC_API_KEY environment variable """
+
     ai_assistant = ClaudeModel('claude-3-5-sonnet-20240620',
                                1000,
                                "You give single sentence answers.")
 
     response = ai_assistant._query_claude(
         "I'm testing my new API. I want you to respond exactly: \
-        'Hello, Brave New AI World.'"
+        'Hello, Brave New AI World. My name is ____ (fill in the blank).'"
                              )
 
     print(response.content)
