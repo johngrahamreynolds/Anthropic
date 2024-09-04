540a1351bcceaba21bab86be68f928d818accf10
John Graham Reynolds <john.graham.reynolds@vumc.org>
2024-09-04 20:55:12 +0000

    add less trivial superstring theory query latex file compilation

--- src/interactive_model/queries/superstring_theory.py
+++ src/interactive_model/queries/superstring_theory.py
@@ -1,21 +1,33 @@
 from src.model_construction import ClaudeModel
+from src.model_construction import LatexParser
+from src.model_construction import MaxTokenException
+import os
 
 if __name__ == "__main__":
 
-    """ update ANTHROPIC_API_KEY environment variable """
-
-    # we must bump the maximum number of tokens for complex queries like that below
-
+    """ 
+        1) update ANTHROPIC_API_KEY environment variable 
+        2) choose the 3 free parameters which characterize the model you query; see the ClaudeModel docstring for accepted values 
+    """
+    
     ai_assistant = ClaudeModel('claude-3-5-sonnet-20240620',
-                               1500,
+                               2500,
                                "You are an AI assistant with expertise in high-energy theoretical physics including string theory, general relativity, \
-                               quantum field theory, and more. You compile all relevant mathematical formulas into an efficient and beautiful format using LaTeX.")
+                               quantum field theory, and more. You compile all relevant mathematical formulas into an efficient and beautiful format using LaTeX. \
+                               Your answers are consistent only of LaTeX code."
+                            )
 
+    
     response = ai_assistant._query_claude(
         "Give me the action for superstring theory including all ghost fields. \
         Show in a step-by-step derivation, thinking carefully through each step, how one acquires the relevant equations of motion by using the principle of least action. \
         Display the results in an easy-to-read LaTeX compiled format. Include this query word for word at the top of LaTeX document for comparative reference. \
-        max_tokens = 1500."
+        max_tokens = 2500."
                              )
-
-    print(response.content)
+    
+    if response.stop_reason == "max_tokens":
+        raise MaxTokenException(ai_assistant.max_tokens)
+        
+    latex_parser = LatexParser()
+    output_file = os.path.basename(__file__)
+    latex_parser.parse_tokens(response, output_file[:-3])
